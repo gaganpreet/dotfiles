0ae90cbb0d6fa32462161eb873b7f4b5c14e8458
Gaganpreet <gaganpreet.arora@gmail.com>
2013-05-12 23:58:40 +0530

    Added histfile for python shell

--- dotfiles/pythonrc.py
+++ dotfiles/pythonrc.py
@@ -1,3 +1,17 @@
 import rlcompleter
 import readline
+import os
+import atexit
+
+hist_file = os.path.expanduser("~/.py_history")
+
+# Load history into memory
+if os.path.exists(hist_file):
+    readline.read_history_file(hist_file)
+
+def save_history(hist_file=hist_file):
+    readline.write_history_file(hist_file)
+atexit.register(save_history)
+
+
 readline.parse_and_bind("tab: complete")
