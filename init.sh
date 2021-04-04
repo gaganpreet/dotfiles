27aa8bb0784ade7daf50d90ee7cd904a66f3748c
Gaganpreet Arora <me@gaganpreet.in>
2021-04-04 11:53:05 +0200

    update init script

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-ln -s $(pwd)/dotfiles/coc-settings.json     $HOME/.config/nvim/coc-settings.json
+mkdir -p $HOME/.config/nvim/ && ln -s $(pwd)/dotfiles/coc-settings.json     $HOME/.config/nvim/coc-settings.json
 ln -s $(pwd)/dotfiles/init.vim              $HOME/.config/nvim/init.vim
+
+mkdir -p $HOME/.config/albert && ln -s $(pwd)/dotfiles/albert.conf           $HOME/.config/albert/albert.conf
+
+ln -s $(pwd)/dotfiles/tmux.conf             $HOME/.tmux.conf
 ln -s $(pwd)/dotfiles/zshrc                 $HOME/.zshrc
-ln -s $(pwd)/dotfiles/albert.conf           $HOME/.config/albert/albert.conf
